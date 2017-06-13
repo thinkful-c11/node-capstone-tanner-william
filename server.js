e089151254446fc30f57dd7ad345533f93e76a18
tannaurus <contacttannergill@gmail.com>
2017-06-13 13:44:00 -0700

    Working put endpoint. About 50% completed.

--- server.js
+++ server.js
@@ -18,74 +18,82 @@ app.use(bodyParser);
 
 app.use(express.static('public'));
 
-app.get("/", (req, res)=>{
+app.get('/', (req, res)=>{
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/tags/evaluate', (req, res)=>{
-  const tag = req.body.tag;
-  const type = req.body.type;
-  const data = JSON.stringify(req.body.data);
-  const url1 = `/tags/${type}`;
-  const url2 = `/tags/${type}/${tag}`;
+// app.post('/tags/evaluate', (req, res)=>{
+//   const tag = req.body.tag;
+//   const type = req.body.type;
+//   const data = JSON.stringify(req.body.data);
+//   const url1 = `http://localhost:8080/tags/${type}`;
+//   const url2 = `http://localhost:8080/tags/${type}/${tag}`;
+//   Stubs
+//     .find({tag})
+//     .count()
+//     .then(count => {
+//       if(count > 0){
+//         return fetch(url1, {method: 'PUT', body: data, 'content-type': 'application/json'});
+//       }else {
+//         return fetch(url2, {method: 'POST', body: data, 'content-type': 'application/json'})
+//         .then(_res => console.log(_res)).catch(err => console.error(err));
+//       }
+//     });
+// });
+
+app.put('/tags/:type/:tag', (req, res)=>{
   Stubs
-    .find({tag})
+    .find({tag: req.params.tag})
     .count()
     .then(count => {
       if(count > 0){
-        fetch(url1, {method: 'PUT', body: data, 'content-type': 'application/json'});
+        console.log('oops that already exists');
       }else {
-        fetch(url2, {method: 'POST', body: data, 'content-type': 'application/json'})
-        .then(_res => console.log(_res)).catch(err => console.error(err));
+        if(req.params.type === 'artist'){
+          console.log('########passing.');
+          Stubs
+            .create({
+              tag: req.params.tag,
+              artists: [{
+                name: req.body.artist.name,
+                id: req.body.artist.id,
+                related: req.body.artist.related,
+                genres: req.body.artist.genres,
+                tags: req.body.artist.tags
+              }],
+              songs: [],
+              albums: [],
+            }).then(_res => res.status(201).json(_res))
+            .catch(err=> {
+              console.error(err);
+              res.sendStatus(500);
+            });
+        }else if(req.params.type === 'song'){
+          Stubs
+            .create({
+              tag: req.body.tag,
+              artist: [{
+                name: req.body.artist.name,
+                id: req.body.artist.id,
+                related: req.body.artist.related,
+                genres: req.body.artist.genres,
+                tags: req.body.artist.tags
+              }],
+              songs: [],
+              albums: [],
+            });
+        }
       }
     });
 });
 
-app.post('/tags/:type/:tag', (req, res)=>{
-  if(req.params.type === 'artist'){
-    console.log('########passing.')
-    Stubs
-    .create({
-      tag: req.params.tag,
-      artists: [{
-        name: req.body.artist.name,
-        id: req.body.artist.id,
-        related: req.body.artist.related,
-        genres: req.body.artist.genres,
-        tags: req.body.artist.tags
-      }],
-      songs: [],
-      albums: [],
-    }).then(_res => res.status(201).json(_res))
-    .catch(err=> {
-      console.error(err);
-      res.sendStatus(500);
-    });
-  }else if(req.params.type === 'song'){
-    Stubs
-    .create({
-      tag: req.body.tag,
-      artist: [{
-        name: req.body.artist.name,
-        id: req.body.artist.id,
-        related: req.body.artist.related,
-        genres: req.body.artist.genres,
-        tags: req.body.artist.tags
-      }],
-      songs: [],
-      albums: [],
-    });
-  }
-    
-});
-
-app.get("/search/:type/:query", (req, clientRespond)=>{
+app.get('/search/:type/:query', (req, clientRespond)=>{
   getCredentials().then(credentials=> {
     sReqBySearch(baseUrl, req.params.type, req.params.query, clientRespond, credentials);
   });
 });
 
-const baseUrl = `https://api.spotify.com/v1/`;
+const baseUrl = 'https://api.spotify.com/v1/';
 
 app.listen(process.env.PORT || 8888, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
@@ -119,15 +127,15 @@ function runServer(databaseUrl=DATABASE_URL, port=PORT) {
 
 function closeServer() {
   return mongoose.disconnect().then(() => {
-     return new Promise((resolve, reject) => {
-       console.log('Closing server');
-       server.close(err => {
-           if (err) {
-               return reject(err);
-           }
-           resolve();
-       });
-     });
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
   });
 }
 
