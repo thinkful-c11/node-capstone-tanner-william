20de26d1fec4f97efd58037fb9e4469c5ae24e67
williamtwobit <william@twobit.cc>
2017-06-13 17:37:37 -0400

    added new schema for aux / relational collections

--- models.js
+++ models.js
@@ -6,13 +6,35 @@ const stubsSchema = mongoose.Schema({
   tag: {type: String, required: true, unique: true},
   artists: [{
     name: String,
-    id: String,
+    id: {type: String, unique: true},
     related: Array,
     genres: Array,
     tags: Array,
   }],
   songs: Array,
   albums: Array,
+});
+
+const artistsSchema = mongoose.Schema({
+  artist: {type: String, required: true, unique: true},
+  tags: Array
+});
+
+const songsSchema = mongoose.Schema({
+  song: {
+    artist: String,
+    album: String,
+    title: String
+  },
+  tags: Array
+});
+
+const albumsSchema = mongoose.Schema({
+  album: {
+    artist: String,
+    title: String,
+  },
+  tags: Array
 });
 
 const currentArtist = {
@@ -37,5 +59,9 @@ function createDbObj() {
   const storage = Object.create(db);
   return storage;
 }
-const model = mongoose.model('Stubs', stubsSchema);
-module.exports = {CurrentArtist: createCurrentArtistObj(), Stubs: model};
+const stubsModel = mongoose.model('Stubs', stubsSchema);
+const artistModel = mongoose.model('Artists', artistsSchema);
+const songModel = mongoose.model('Songs', songsSchema);
+const albumModel = mongoose.model('Albums', albumsSchema);
+
+module.exports = {CurrentArtist: createCurrentArtistObj(), Stubs: stubsModel, Artists: artistsSchema, Songs: songsSchema, Albums: albumsSchema};
